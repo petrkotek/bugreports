6324e07dfff302ff112914b8453c0477b2067d31
Petr Kotek <kotekp@gmail.com>
2024-04-24 13:41:40 +1000

    Add code reproducing vue eslint issue.

---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  methods: {
+    save() {}
+  }
+}
+</script>
+
+<template>
+  <!-- OK -->
+  <input @keydown.ctrl.enter.exact="save()" />
+  <!-- OK -->
+  <input @keydown.stop />
+  <!-- ERROR - Fails with vue/use-v-on-exact -> Consider to use '.exact' modifier on the @keydown.stop line -->
+  <input
+    @keydown.ctrl.enter.exact="save()"
+    @keydown.stop
+  />
+</template>
